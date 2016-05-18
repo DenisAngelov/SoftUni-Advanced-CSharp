6c49603257246afe33fac848414a49b968304fbd
DenisAngelov <denis_angelov@yahoo.com>
2016-05-18 19:01:06 +0300

    Added Stack and Queues Homework

---
+++ 01.BasicStackOperations/01.ReverseNumbersWithStack/ReverseNumsStack.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ReverseNumsStack
+{
+    public static void Main(string[] args)
+    {
+        Stack<int> numbers = new Stack<int>(Console.ReadLine().Split(new [] {' '}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+        Console.WriteLine(String.Join(" ", numbers));
+    }
+}
---
+++ 01.BasicStackOperations/04.BasicQueueOperations/BasicQueue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class BasicQueue
+{
+    public static void Main(string[] args)
+    {
+        int[] data = Console.ReadLine().Split(' ').Skip(1).Select(int.Parse).ToArray();
+        Queue<int> numbers = new Queue<int>(Console.ReadLine().Split(' ').Select(int.Parse));
+
+        for (int remove = 0; remove < data[0]; remove++)
+            numbers.Dequeue();
+
+        if (numbers.Count == 0)
+            Console.WriteLine(0);
+        else if (!numbers.Contains(data[1]))
+            Console.WriteLine(numbers.Min());
+        else
+            Console.WriteLine("true");
+    }
+}
